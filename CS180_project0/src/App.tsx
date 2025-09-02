6ff21ab73e1af9c2ee56386a4ec9bcf37bb2bba1
Maximilian Christof <max@Maximilians-MacBook-Pro.local>
2025-09-01 23:21:25 -0700

    routing error fix3

--- CS180_project0/src/App.tsx
+++ CS180_project0/src/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
-
 const queryClient = new QueryClient();
 
 const App = () => {
@@ -28,3 +27,5 @@ const App = () => {
     </QueryClientProvider>
   );
 };
+
+export default App;
